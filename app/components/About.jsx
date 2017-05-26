c754a9288498af365ce9735a6465c5579290e57d
Dylan Hobbs <dylan@onemethod.com>
2017-05-25 23:36:33 -0400

    add styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,17 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About Component</h3>
-      <p>Welcome to the about page</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather applicaiton build on React.</p>
+      <p>Here are some of the tools I used:</p>
+      <ul>
+        <li>
+          <a href="http://facebook.github.io/react">React</a> – This is the front-end framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> – I used Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
     </div>
   )
 };
